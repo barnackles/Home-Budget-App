90376366b9a65045ae05870520571c6cb904a117
barnackles <p.martewicz@yahoo.com>
2022-08-15 20:06:34 +0200

    Budget create - added "data" for responseDto

--- src/main/java/com/barnackles/budget/BudgetResponseDto.java
+++ src/main/java/com/barnackles/budget/BudgetResponseDto.java
@@ -3,10 +3,12 @@ package com.barnackles.budget;
 
 import com.barnackles.operation.Operation;
 import com.barnackles.user.User;
+import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Id;
 import java.util.List;
 
+@Data
 public class BudgetResponseDto {
     @Id
     private Long id;
