7dc66eca297b58e73894975d1720be1883a17f2f
barnackles <p.martewicz@yahoo.com>
2022-08-19 22:01:38 +0200

    Budget controller separation

--- src/main/java/com/barnackles/budget/admin/BudgetAdminRestController.java
+++ src/main/java/com/barnackles/budget/admin/BudgetAdminRestController.java
@@ -1,6 +1,9 @@
-package com.barnackles.budget;
+package com.barnackles.budget.admin;
 
-import com.barnackles.ApplicationSecurity.IAuthenticationFacade;
+import com.barnackles.budget.Budget;
+import com.barnackles.budget.BudgetCreateDto;
+import com.barnackles.budget.BudgetResponseDto;
+import com.barnackles.budget.BudgetService;
 import com.barnackles.user.User;
 import com.barnackles.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +11,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,40 +18,32 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/budget")
-public class BudgetRestController {
+@RequestMapping("/admin/budget")
+public class BudgetAdminRestController {
 
     private final BudgetService budgetService;
     private final ModelMapper modelMapper;
-
     private final UserService userService;
 
-    private final IAuthenticationFacade authenticationFacade;
-
-
-
-    @PreAuthorize("hasRole('ROLE_USER')")
-    @GetMapping("/budgets/current-user")
-    public ResponseEntity<List<BudgetResponseDto>> findAllUserBudgets() {
-
-       Authentication authentication = authenticationFacade.getAuthentication();
-
-       List<Budget> userBudgetList = userService.findUserByUserName(authentication.getName()).getBudgets();
-       List<BudgetResponseDto> listOfBudgetResponseDtos = userBudgetList
+    //add pagination
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/budgets/all")
+    public ResponseEntity<List<BudgetResponseDto>> findAll() {
+        List<Budget> budgets = budgetService.findAll();
+        List<BudgetResponseDto> listOfBudgetResponseDtos = budgets
                 .stream()
                 .map(this::convertBudgetResponseDto)
                 .toList();
         return new ResponseEntity<>(listOfBudgetResponseDtos, HttpStatus.OK);
     }
 
-    //findbudgetbyname
-
-    @PostMapping("/budget")
-    public ResponseEntity<BudgetResponseDto> createBudget(@RequestBody BudgetCreateDto budgetCreateDto) {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/budget/{id}")
+    public ResponseEntity<BudgetResponseDto> createBudget(@RequestBody BudgetCreateDto budgetCreateDto, @PathVariable Long id) {
         Budget budget = convertCreateDtoToBudget(budgetCreateDto);
-        Authentication authentication = authenticationFacade.getAuthentication();
 
-        User user = userService.findUserByUserName(authentication.getName());
+
+        User user = userService.findUserById(id);
 
         List<User> usersList = new ArrayList<>();
         usersList.add(user);
